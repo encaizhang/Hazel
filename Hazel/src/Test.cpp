7a03e14db28d858c37101e53373c135cf136078e
zec <c1002929112@163.com>
2022-04-05 21:17:56 +0800

    initial commit

---
+++ Hazel/src/Test.cpp
@@ -0,0 +1,7 @@
+#include "Test.h"
+
+namespace Hazel {
+	void Print() {
+		std::cout << "welcome to hazel render engine" << std::endl;
+	}
+}
---
+++ Hazel/src/Test.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+namespace Hazel{
+
+	__declspec(dllexport) void Print();  
+}
+
